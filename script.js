279a9ebb9c96c31d9f7c7369b236a2289f0d63c1
Dalila-Queiroz <dalila.queiroz84@gmail.com>
2026-05-22 20:17:41 -0500

    Ref. Atual 10

---
+++ script.js
@@ -0,0 +1,82 @@
+console.log("Boa noite, hoje é sexta-feira!");
+
+// console
+
+console.error("Ops, algo deu errado!");
+
+console.warn("Cuidado, isso pode ser perigoso!");   
+
+// variáveis
+var nome = "Dalila";
+var idade = 41;
+var altura = 1.50;
+var estudante = true;   
+
+var x = 10
+
+// variaveis modernas
+
+let y = 15
+
+const z = 20
+
+console.log(x)
+console.log(y)
+console.log(z)
+
+// tipos de dados
+
+// texto
+var nome = "Dalila Regina da SilvaQueiroz";
+
+console.log(nome);
+
+console.log(typeof nome);
+
+// número
+
+const shirtsQty = 4;
+console.log(shirtsQty);
+console.log(typeof shirtsQty);
+
+const isStudent = true;
+console.log(isStudent);
+console.log(typeof isStudent);  
+
+const decimal = 3.14;
+console.log(decimal);
+console.log(typeof decimal);
+
+//boolean (true ou false)
+const isAdult = true;
+console.log(isAdult);
+console.log(typeof isAdult);
+
+//null
+let surname = null;
+console.log(surname);
+console.log(typeof surname);
+
+// Arrays - guarda os valores em uma lista ordenada
+let languages = ["JavaScript", "Python", "Java"];
+console.log(languages);
+console.log(typeof languages); 
+
+// objetos - guarda os valores em uma estrutura de chave-valor
+const user = {email:"dalila.queiroz84@gmail.com", password: "tente", age: 20};
+console.log(user);
+console.log(typeof user);
+
+//método de string
+const fullName = "Dalila Regina da Silva Queiroz";
+console.log(fullName.length);
+console.log(typeof fullName);
+
+const stringToArray = fullName.split(" ");
+console.log(stringToArray);
+console.log(typeof stringToArray);  
+console.log(fullName.toUpperCase());
+console.log(fullName.toLowerCase());    
+console.log(fullName.trim());
+
+
